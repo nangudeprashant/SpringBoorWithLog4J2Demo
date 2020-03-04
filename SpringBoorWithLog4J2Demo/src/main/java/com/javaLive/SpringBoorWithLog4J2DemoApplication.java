e12fc787d6f54a6be47c6c8e5b4a33e55c61f49b
Administrator <Administrator@DELL>
2020-03-04 18:42:53 +0530

    Initial commit.

---
+++ SpringBoorWithLog4J2Demo/src/main/java/com/javaLive/SpringBoorWithLog4J2DemoApplication.java
@@ -0,0 +1,28 @@
+package com.javaLive;
+
+import org.apache.log4j.BasicConfigurator;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringBoorWithLog4J2DemoApplication {
+
+	//private static final Logger LOGGER = LoggerFactory.getLogger(SpringBoorWithLog4J2DemoApplication.class);
+	private static Logger LOGGER = LogManager.getLogger(SpringBoorWithLog4J2DemoApplication.class);
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		SpringApplication.run(SpringBoorWithLog4J2DemoApplication.class, args);
+		LOGGER.info("=============================Starting Spring Boot application....................");
+	    LOGGER.info("*******************Info level log message***********************");
+        LOGGER.debug("******************Debug level log message**********************");
+        LOGGER.error("******************Error level log message**********************");
+        
+	}
+
+}
---
+++ SpringBoorWithLog4J2Demo/src/test/java/com/javaLive/SpringBoorWithLog4J2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaLive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoorWithLog4J2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
