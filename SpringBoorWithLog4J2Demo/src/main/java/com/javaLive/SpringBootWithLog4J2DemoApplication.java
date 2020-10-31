9ed0319c5b92242b3ee7409f19bc1bb3bd191304
Administrator <Administrator@DELL>
2020-10-31 15:36:36 +0530

    Project guide file is added.

--- SpringBoorWithLog4J2Demo/src/main/java/com/javaLive/SpringBootWithLog4J2DemoApplication.java
+++ SpringBoorWithLog4J2Demo/src/main/java/com/javaLive/SpringBootWithLog4J2DemoApplication.java
@@ -1,6 +1,6 @@
 package com.javaLive;
 
-import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.BasicConfigurator;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 import org.apache.logging.log4j.LogManager;
@@ -11,13 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class SpringBoorWithLog4J2DemoApplication {
+public class SpringBootWithLog4J2DemoApplication {
 
 	//private static final Logger LOGGER = LoggerFactory.getLogger(SpringBoorWithLog4J2DemoApplication.class);
-	private static Logger LOGGER = LogManager.getLogger(SpringBoorWithLog4J2DemoApplication.class);
+	private static Logger LOGGER = LogManager.getLogger(SpringBootWithLog4J2DemoApplication.class);
 	public static void main(String[] args) {
-		BasicConfigurator.configure();
-		SpringApplication.run(SpringBoorWithLog4J2DemoApplication.class, args);
+		//BasicConfigurator.configure();
+		SpringApplication.run(SpringBootWithLog4J2DemoApplication.class, args);
 		LOGGER.info("=============================Starting Spring Boot application....................");
 	    LOGGER.info("*******************Info level log message***********************");
         LOGGER.debug("******************Debug level log message**********************");
